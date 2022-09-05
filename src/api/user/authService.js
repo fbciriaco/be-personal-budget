3025245d7924a587d545abfa0433b6709104f330
fbciriaco <fb.ciriaco@gmail.com>
2022-09-05 15:37:10 -0300

    AuthService

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt')
 const User = require('./user')
 const env = require('../../.env')
 
-const emaiRegex = /\S+@\S+\.\S+/
+const emailRegex = /\S+@\S+\.\S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z](?=.*[@#$%]).{6,20})/
 
 const sendErrorsFromDB = (res, dbErrors) => {
@@ -47,7 +47,65 @@ const login = (req, res, next) => {
 const validateToken = (req, res, next) => {
     const token = req.body.token || ''
 
-    jwt.verify(token, env.authSecret, function (err, decoded){
-        return res.status(200).send({ valid: !err})
+    jwt.verify(token, env.authSecret, function (err, decoded) {
+        return res.status(200).send({
+            valid: !err
+        })
     })
 }
+
+const signup = (req, res, any) => {
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.body.confirm_password || ''
+
+    if (!email.match(emailRegex)) {
+        return res.status(400).send({
+            errors: ['O e-mail informado está inválido']
+        })
+    }
+
+    if (!password.match(passwordRegex)) {
+        return res.status(400).send({
+            errors: [
+                'A senha precisa ter uma letra maiúscula, uma letra minúscula, um caractere especial e entre 6 e 20 caracteres'
+            ]
+        })
+    }
+
+    const salt = bcrypt.genSaltSync()
+    const passwordHash = bcrypt.hashSync(password, salt)
+    if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
+        return res.status(400).send({
+            errors: ['Senhas não conferem']
+        })
+    }
+
+    User.findOne({
+        email
+    }, (err, user) => {
+        if (err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user) {
+            return res.status(400).send({
+                errors: ['Usuário já cadastrado']
+            })
+        } else {
+            const newUser = new User({
+                name,
+                email,
+                password: passwordHash
+            })
+            newUser.save(err => {
+                if (err) {
+                    return sendErrorsFromDB(res, err)
+                } else {
+                    login(req, res, next)
+                }
+            })
+        }
+    })
+}
+
+module.exports = {login, signup, validateToken}
